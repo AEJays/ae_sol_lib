520177cb4150c9a78436806fd73c186c71a27293
AEdgeJay <471021444@qq.com>
2022-03-14 16:51:43 +0800

    add permission

---
+++ Permission/Permission.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";  //日志
+
+abstract contract Permission{
+    address owner; //所有者 默认为合约本身
+    struct bannerMes {
+        uint startTime;
+        uint endTime;
+    }
+    struct bannerType{
+        string _type;
+        address banner;
+        uint startTime;
+        uint endTime;
+        bool isBan;
+    }
+    mapping(address=>bool) admins; //管理员组校验
+    mapping(address=>bool) isBanner; //是否被ban了
+    mapping(address=>bannerMes) isTempBanner; //临时ban禁
+    bannerType[] bannerGroup;
+    modifier isOwner(){
+        console.log(msg.sender,owner);
+        require(msg.sender == owner||owner == address(0),"You don't have permission");
+        _;
+    }
+    modifier isBan(){
+        if(isBanner[msg.sender]){
+            revert("You are permanently banned");
+        }else if(isTempBanner[msg.sender].startTime<=block.timestamp&&isTempBanner[msg.sender].endTime>=block.timestamp){
+            revert("You have been temporarily banned");
+        }
+        _;
+    }
+    // 所有者默认包含owner
+    modifier isAdmin(){
+        require(admins[msg.sender],"You are not an administrator");
+        _;
+    }
+    // 设置所有者
+    function setOwner(address _owner) public payable isOwner{
+        owner = _owner;
+        admins[owner] = true;
+    }
+    // 获取所有者
+    function getOwner() public view isOwner returns (address _owner){
+        return owner;
+    }
+    // 设置管理员 除了owner外的管理员 可执行ban禁玩家的操作 但不是权限所有者 只有所有者可设置
+    function setAdmin(address _admin) public payable isOwner{
+        admins[_admin] = true;
+    }
+    // 取消管理员 所有者才可设置
+    function removeAdmin(address _admin) public payable isOwner{
+        admins[_admin] = false;
+    }
+    // 设置永久ban禁地址
+    function setBanner(address _banner) public payable isAdmin{
+        isBanner[_banner] = true;
+        bannerGroup.push(bannerType("permanent",_banner,block.timestamp,0,true));
+    }
+    // 解除永久ban禁
+    function removeBanner(address _banner) public payable isAdmin{
+        isBanner[_banner] = false;
+        bool hasBanner = false;
+        for(uint i = 0;i<bannerGroup.length;i++){
+            if(bannerGroup[i].banner == _banner){
+                hasBanner = true;
+                if(keccak256(abi.encodePacked(bannerGroup[i]._type)) == keccak256(abi.encodePacked("permanent"))){
+                     if(bannerGroup[i].isBan){
+                        bannerGroup[i].isBan = false;
+                    }else{
+                        revert("Users are not banned");
+                    }
+                }else{
+                    revert("Type of ban error");
+                }
+            }
+        }
+        if(hasBanner == false){
+            revert("Players are not on the ban list");
+        }
+    }
+    // 获取临时ban禁的地址及时间
+    function setTempBanner(address _banner,uint _startTime,uint _endTime) public payable isAdmin{
+        isTempBanner[_banner] = bannerMes(_startTime,_endTime);
+        bannerGroup.push(bannerType("temporary",_banner,_startTime,_endTime,true));
+    }
+    // 解除临时ban禁
+    function removeTempBanner(address _banner) public payable isAdmin{
+        isTempBanner[_banner] = bannerMes(0,0);
+        bool hasBanner = false;
+        for(uint i = 0;i<bannerGroup.length;i++){
+            if(bannerGroup[i].banner == _banner){
+                hasBanner = true;
+                if(keccak256(abi.encodePacked(bannerGroup[i]._type)) == keccak256(abi.encodePacked("temporary"))){
+                    if(bannerGroup[i].isBan){
+                        bannerGroup[i].isBan = false;
+                    }else{
+                        revert("Users are not banned");
+                    }
+                }else{
+                    revert("Type of ban error");
+                }
+            }
+        }
+        if(hasBanner == false){
+            revert("Players are not on the ban list");
+        }
+    }
+    // 获取所有ban禁地址
+    function getBanner() public view isAdmin returns (bannerType[] memory banners){
+        return bannerGroup;
+    }
+}
--- README.md
+++ README.md
@@ -27,6 +27,7 @@ MathLib.Random(uint 前端传入的随机数,uint8 长度) 随机数
 
 #### timeStamp
 引入方式
+
 在合约头增加以下代码
 
 ```sol
@@ -41,3 +42,57 @@ timeStamp.formatTime(uint 时间戳)
 ```bash
 npm i solidity_lib
 ```
+
+#### Permission
+引入方式
+
+在合约头增加以下代码
+
+```sol
+import 'solidity_lib/Permission/Permission.sol';
+```
+
+继承权限库
+
+```sol
+contract xxx is Permission{
+    // 默认不设置 owner 为0地址 需要在初始化时候设置所有者
+    constructor(address owner){
+        setOwner(owner);
+    }
+}
+```
+
+权限校验 
+
+```sol
+// 该方法只允许管理员使用 注意 所有者默认为管理员
+function ff(address user) public payable isAdmin{
+}
+// 该方法只允许所有者使用
+function ff(address user) public payable isOwner{
+}
+// 该方法只允许未被禁用的玩家使用
+function ff(address user) public payable isBan{
+}
+```
+
+现有方法
+
+setOwner(address) 设置所有者 权限只开放给所有者
+
+getOwner() 获取所有者 权限只开放给所有者
+
+setAdmin(address) 设置管理员 权限只开放给所有者
+
+removeAdmin(address) 删除管理员 权限只开放给所有者
+
+setBanner(address) 设置永久禁用的地址 权限开放给所有者及管理员
+
+removeBanner(address) 删除永久禁用的地址(解封) 类型需要对 权限开放给所有者及管理员
+
+setTempBanner(address,uint,uint) 设置临时禁用的地址 需要设置开始时间至结束时间 权限开放给管理员
+
+removeTempBanner(address) 删除临时禁用的地址(解封) 类型需要对
+
+getBanner() 获取封禁列表 isBan 为true时 表示已被封禁 为false表示已解禁 
